afddc5cad86970d99cdc98f35b036bac95a0ce15
vaisakhsysol <vaisakhsysol@gmail.com>
2023-11-19 19:22:23 +0530

    Micro_Interactions

---
+++ Js/app.js
@@ -0,0 +1,16 @@
+const hamburgerMain = document.querySelector('.hamburgerMain')
+const navLinks = document.querySelector('.navLinks')
+const navLink = document.querySelector('.navLink')
+
+
+hamburgerMain.addEventListener('click',()=>{
+    hamburgerMain.classList.toggle('active')
+    navLinks.classList.toggle('active')
+})
+
+navLink.forEach(link => {
+    link.addEventListener('click',()=>{
+        hamburgerMain.classList.remove('active')
+        navLinks.classList.remove('active')
+    })
+});
--- Js/indication.js
+++ Js/indication.js
@@ -1,5 +1,5 @@
 const activePage = window.location.pathname
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
+const naviLinks = document.querySelectorAll('nav a').forEach(link => {
     if (link.href.includes(`${activePage}`)) {
         link.classList.add('active')
     }
