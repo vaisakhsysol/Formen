2297c8189e217a9f97d8f67f20486a38d503e466
vaisakhp98 <vaisakhp43@gmail.com>
2023-11-29 12:13:17 +0530

    updatedContent

--- Js/indication.js
+++ Js/indication.js
@@ -1,6 +1,12 @@
 const activePage = window.location.pathname
+
 const naviLinks = document.querySelectorAll('nav a').forEach(link => {
     if (link.href.includes(`${activePage}`)) {
         link.classList.add('active')
     }
+
+    if (activePage == '/') {
+        naviLinks.classList.remove('active')
+    }
 });
+
